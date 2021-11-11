fa65f8bbbca94f36407bf256cfc04100e10178e9
Solomon Negusse <solomon.negusse@wri.org>
2021-11-11 17:04:35 -0600

    add query endpoint

--- app/src/routes/api/v1/gfw.router.ts
+++ app/src/routes/api/v1/gfw.router.ts
@@ -1,6 +1,7 @@
 import { Context, Next } from 'koa';
 import Router from 'koa-router';
-import { RWAPIMicroservice } from 'rw-api-microservice-node';
+import { RWAPIMicroservice, RequestToMicroserviceOptions} from 'rw-api-microservice-node';
+import type request from 'request';
 
 import FieldSerializer from 'serializers/field.serializer';
 import logger from 'logger';
@@ -10,7 +11,6 @@ import GfwService from 'services/gfw.service';
 const router: Router = new Router({
     prefix: `/api/${process.env.API_VERSION}/gfw`,
 });
-
 
 class GfwRouter {
 
@@ -48,11 +48,94 @@ class GfwRouter {
         logger.info('Obtaining fields');
         const fields: Record<string, any> = await GfwService.getFields(ctx.request.body.dataset.connectorUrl);
         ctx.body = FieldSerializer.serialize(fields.data);
-    }   
+    }
+
+    static async query(ctx: Context): Promise<void> {
+        logger.info('Executing query');
+        // @ts-ignore
+        const queryResults: Record<string, any> = await GfwService.executeQuery(
+            ctx.request.body.dataset.connectorUrl, ctx.query.sql);
+        ctx.body = queryResults;
+    }
 }
+
+const serializeObjToQuery: (obj: Record<string, any>) => string = (obj: Record<string, any>) => Object.keys(obj).reduce((a, k) => {
+    a.push(`${k}=${encodeURIComponent(obj[k])}`);
+    return a;
+}, []).join('&');
+
+
+const toSQLMiddleware:(ctx: Context, next: Next) => Promise<void> = async (ctx: Context, next: Next) => {
+    const options: RequestToMicroserviceOptions & request.OptionsWithUri = {
+        method: 'GET',
+        json: true,
+        resolveWithFullResponse: true,
+        simple: false,
+        uri: '',
+    };
+    if (!ctx.query.sql && !ctx.request.body.sql) {
+        ctx.throw(400, 'sql or fs required');
+        return;
+    }
+
+    if (ctx.query.sql || ctx.request.body.sql) {
+        const params: Record<string, any> = { ...ctx.query, ...ctx.request.body };
+        options.uri = `${process.env.API_VERSION}/convert/sql2SQL?sql=${encodeURIComponent(params.sql)}&experimental=true&raster=${ctx.request.body.dataset.type === 'raster'}`;
+        logger.debug(`Checking sql correct: ${params.sql}`);
+
+        if (params.geostore) {
+            options.uri += `&geostore=${params.geostore}`;
+        }
+        if (params.geojson) {
+            options.body = {
+                geojson: params.geojson
+            };
+            options.method = 'POST';
+        }
+    } else {
+        logger.debug('Obtaining sql from featureService');
+        const fs: Record<string, any> = { ...ctx.request.body };
+        delete fs.dataset;
+        const query: string = serializeObjToQuery(ctx.request.query);
+        const body: Record<string, any> = fs;
+
+        if (query) {
+            options.uri = `/convert/fs2SQL${query}&tableName=data`;
+        } else {
+            options.uri = `/convert/fs2SQL?tableName=data`;
+        }
+        options.body = body;
+        options.method = 'POST';
+    }
+
+    try {
+        const result: Record<string, any> = await RWAPIMicroservice.requestToMicroservice(options);
+        logger.debug('result', result.statusCode);
+        if (result.statusCode === 204 || result.statusCode === 200) {
+            ctx.query.sql = result.body.data.attributes.query;
+            ctx.state.jsonSql = result.body.data.attributes.jsonSql;
+            await next();
+        } else if (result.statusCode === 400) {
+            ctx.status = result.statusCode;
+            ctx.body = result.body;
+        } else {
+            ctx.throw(result.statusCode, result.body);
+        }
+
+    } catch (e) {
+        if (e.errors && e.errors.length > 0 && e.errors[0].status >= 400 && e.errors[0].status < 500) {
+            ctx.status = e.errors[0].status;
+            ctx.body = e;
+        } else {
+            throw e;
+        }
+    }
+};
 
 router.get('/fields/:dataset', DatasetMiddleware.getDatasetById, GfwRouter.fields);
 router.post('/rest-datasets/gfw', GfwRouter.registerDataset);
+router.get('/query/:dataset', DatasetMiddleware.getDatasetById, toSQLMiddleware, GfwRouter.query);
+router.post('/query/:dataset', DatasetMiddleware.getDatasetById, toSQLMiddleware, GfwRouter.query);
 
 
 export default router;
--- app/src/services/gfw.service.ts
+++ app/src/services/gfw.service.ts
@@ -17,6 +17,18 @@ class GfwService {
             throw new Error('Error obtaining fields');
         }
     }
+
+    static async executeQuery(urlDataset: string, sqlQuery: string): Promise<Record<string, any>> {
+        const reqUrl: string = `${urlDataset}/query?sql=${sqlQuery}`;
+        logger.debug(`Sending query request to ${reqUrl}`);
+        try {
+            const result: Record<string, any> = await axios.get(reqUrl);
+            return result.data;
+        } catch (err) {
+            logger.error('Error obtaining query', err);
+            throw new Error('Error obtaining query results');
+        }
+    }
 }
 
 export default GfwService;
