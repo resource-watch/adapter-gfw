bf662272f22e613c97fc98ac0988466de586028d
Solomon Negusse <solomon.negusse@wri.org>
2021-12-06 15:29:49 -0600

    remove grunt

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        exec grunt --gruntfile app/Gruntfile.js | bunyan
+        exec yarn watch | bunyan -l debug
         ;;
     startDev)
         echo "Running Start Dev"
